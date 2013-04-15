0f56f1e7a6aa3d222521699d8c47b1716fa5c2d5
Guillaume <guillaume.jornod@gmail.com>
2013-04-15 09:50:23 +0200

    Couleur test

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <html>
 <head>
 	<title>Test</title>
+<LINK href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-Première ligne du site ! C'est trop bien
+<div class='red'>Première ligne du site ! C'est trop bien</div>
 </body>
 </html>
