8a6c3db8a01317b4ced144c1cc726ebf1fccddde
Guillaume <guillaume.jornod@gmail.com>
2013-04-15 09:47:01 +0200

    Modif 1

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-	include('connect.php');
+//	include('connect.php');
 ?>
 <html>
 <head>
 	<title>Test</title>
 </head>
 <body>
-Première ligne du site !
+Première ligne du site ! C'est trop bien
 </body>
+</html>
