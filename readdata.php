16acd6d5883faba7ea148f90645b194de6997634
guillaume <guillaume.jornod@gmail.com>
2013-04-18 12:55:39 +0200

    Charset change

--- readdata.php
+++ readdata.php
@@ -142,7 +142,6 @@ unset($athlete[0]);
 
 if ($reimport || $conn->query("SELECT COUNT(*) FROM Athlete")->fetchColumn() == 0 || 1)
 {
-	$ct = 1;
 	foreach ($athlete as $athletee)
 	{
 		$athl = $athletee[0];
@@ -153,13 +152,13 @@ if ($reimport || $conn->query("SELECT COUNT(*) FROM Athlete")->fetchColumn() == 
 			echo $encoAthl . ": ";
 			$athl = mb_convert_encoding($athl, "UCS-2", $encoAthl);
 			echo  mb_detect_encoding($athl, $ary). " - $athl <br>";*/
-			$conn->prepare("INSERT INTO Athlete (aid, aname) VALUES (?, ?)")->execute(array($AID, $athl));
+			echo "$athl: ";
+			$athl = utf8_encode($athl);
+			echo "$athl<br>";
+			$conn->prepare("INSERT INTO Athlete (aid, aname) VALUES (N?, N?)")->execute(array($AID, $athl));
 		}
 		elseif ($debug)
 			echo $athl;
-		$ct ++;
-		if ($ct > 5)
-			break;
 	}
 }
 /*
