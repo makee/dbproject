3db178979e076e556e3733a18ee6b2734de09b14
guillaume <guillaume.jornod@gmail.com>
2013-04-19 19:43:57 +0200

    back to xml

--- queries.php
+++ queries.php
@@ -1,5 +1,5 @@
 <?
-//header("Content-Type: text/xml");
+header("Content-Type: text/xml");
 include_once('connect.php');
 include_once('class.php');
 
@@ -10,22 +10,26 @@ if (isset($_POST['action']) && $_POST['action'] == "top10" && isset($_POST['type
 		$_POST['limit'] = 10;
 	$limit = $_POST['limit'];
 	$res = Athlete::getAthlete($limit);		
-//	$dom = new DomDocument("1.0", "UCS-2");
-//	$root = $dom->createElement('result');
+	$dom = new DomDocument("1.0", "UCS-2");
+	$root = $dom->createElement('result');
 	$list = array();
 	foreach($res as $re)
 	{
-		$list[] = $re->listAttrib();
+		$athl = $dom->createElement('athlete');
+		//$list[] = $re->listAttrib();
+		foreach($re as $key => $attrib)
 		{
-//			$attrib = utf8_encode($attrib);
-//			$elem = $dom->createElement($key, $attrib);
-//			$root->appendChild($elem);
+			$attrib = utf8_encode($attrib);
+			$elem = $dom->createElement($key, $attrib);
+			$athl->appendChild($elem);
 		}
+		$root->appendChild($athl);
 	}
 
-//	$dom->appendChild($root);
-//	$xmlData = $dom->saveXML($root);
-	echo json_encode($list);
+	$dom->appendChild($root);
+	$xmlData = $dom->saveXML($root);
+	//echo json_encode($list);
+	echo $xmlData;
 }
 
 ?>
