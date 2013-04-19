925ac10f7d3f5133b2b226e4daa75f1e967c8e67
guillaume <guillaume.jornod@gmail.com>
2013-04-19 21:02:23 +0200

    Discipline import

---
+++ correctDiscipline.php
@@ -0,0 +1,61 @@
+<?
+include_once('connect.php');
+include_once('functions.php');
+
+$reimport = true;
+$debug = !true;
+if ($reimport || $conn->query("SELECT COUNT(*) FROM Discipline")->fetchColumn() == 0)
+{
+	$ct = 1;
+	if ($debug)
+		echo "<table><tr><th>Discipline</th><th>Gender</th><th>Min Weight</th><th>Max Weight</th><th>Wunit</th><th>Distance</th><th>Dunit</th><th>Team ? </th><th>Category</th><th>Rest</th><th>Sport</th>";
+	foreach ($disciplines as $discipline)
+	{
+		$sportmatch = $conn->query("SELECT sid FROM Sport WHERE sname LIKE '$discipline[1]'")->fetchColumn(); 
+		/*if ($sportmatch != NULL )
+		{
+			$DID = IDgen($discipline[0], "discipline", "did", true);
+		//	$conn->prepare("INSERT INTO Discipline (did, dname, sid) VALUES ('$DID', ?, '$sportmatch')")->execute(array($discipline[0]));
+		}*/
+		$exp = explodeDiscipline($discipline);
+		if ($debug)
+		{
+			echo "<tr>";
+			echo "<td>$discipline[0]</td>";
+			echo "<td>$exp[0]</td>";
+			echo "<td>$exp[1]</td>";
+			echo "<td>$exp[2]</td>";
+			echo "<td>$exp[3]</td>";
+			echo "<td>$exp[4]</td>";
+			echo "<td>$exp[5]</td>";
+			echo "<td>$exp[6]</td>";
+			echo "<td>$exp[7]</td>";
+			echo "<td>$exp[8]</td>";
+			echo "<td>";
+			echo $sportmatch?$sportmatch:$discipline[1];
+			echo "</td>";
+			echo "</tr>";
+		}
+		else
+		{
+			$DID = IDgen($exp['drest'], "discipline", "did", true);
+			foreach ($exp as $key => $val)
+			{
+				$$key = $val;
+			}
+			$exec = array_merge((array)$DID, $exp, (array)$sportmatch);
+			echo "<pre>";
+			var_dump($exec);
+			echo "</pre>";
+			$query =  "INSERT INTO Discipline (did, dgender, dminweight, dmaxweight, dwunit, ddist, ddunit, dteam, dcat, dname, sid) VALUES ('$DID', '$dgender', '$dminweight', '$dmaxweight', '$dwunit', '$ddist', '$ddunit', '$dteam', '$dcat', '$drest', '$sportmatch')";
+			$conn->query($query);
+			//echo $conn->prepare("INSERT INTO Discipline (did, dgender, dminweight, dmaxweight, dwunit, ddist, ddunit, dteam, dcat, dname, sid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")->execute($exec)?'oui':'non';
+		}
+		$ct ++;
+//		if($ct > 1) break;
+	}
+	if ($debug)
+		echo "</table>";
+}
+
+?>
---
+++ functions.php
@@ -0,0 +1,232 @@
+<?
+include_once('connect.php');
+include_once('class.php');
+
+
+
+function readCSV($csvFile)
+{
+	$file_handle = fopen($csvFile, 'r');
+	while (!feof($file_handle) ) 
+	{
+		$line_of_text[] = fgetcsv($file_handle, 1024, ";");
+	}
+	fclose($file_handle);
+	return $line_of_text;
+}
+
+
+function IDgen($orig, $table, $field, $spe=false) 
+{ 
+	global $conn;
+	$unique_ref_length = $spe?7:3;  
+	$unique_ref_found = false;  
+	$possible_chars = "23456789BCDFGHJKMNPQRSTVWXYZ";  
+	$orig = preg_replace('/ /', '', $orig);
+    $IDText = !$spe?strtoupper(substr($orig,0,3)). '-':"";
+	while (!$unique_ref_found) {  
+    	$unique_ref = $IDText;  
+    	$i = 0;  
+    	while ($i < $unique_ref_length) {  
+        	$char = substr($possible_chars, mt_rand(0, strlen($possible_chars)-1), 1);  
+        	$unique_ref .= $char;  
+        	$i++;  
+    	}  
+    	$query = "SELECT $field FROM $table WHERE $field='?'";  
+    	$result = $conn->prepare($query);  
+		$result->execute(array($unique_ref));
+    	if ($result->rowCount() == 0) 
+		{  
+	        $unique_ref_found = true;  
+    	}  
+	}  
+    return $unique_ref;
+}
+
+$dir = "data";
+if (is_dir($dir)) 
+{
+	chdir($dir);
+	foreach (glob("*.csv") as $file) 
+	{
+		//if($file == 'medals.csv' || $file == 'participants.csv'){
+		if($file == 'Disciplines.csv'){
+		//if(1){
+			$path_parts = pathinfo($file); 
+  		$filename = $path_parts['filename']; 
+		$filename = strtolower($filename);
+			$$filename = readCSV($file);
+    }
+	}
+}
+
+
+function explodeDiscipline($disc){
+	// GENDER
+	$spo = $disc[1];
+	$disc = $disc[0];
+	if (!preg_match('/men/i', $disc))
+		$gender = '0';
+	if (preg_match('/Men/', $disc))
+		$gender = '1';
+	if (preg_match('/Women/i', $disc))
+		$gender = '2';
+	if ($gender != '?')
+		$rest = preg_replace('/Men\'s ?|Women\'s ?/', '', $disc);
+	else
+		$rest = $disc;
+	
+	// Weight
+	preg_match('/([0-9.]{0,5}) ?(\+|-)? ?([(0-9.]{1,5}) ?(kg)/', $rest, $weight);
+	$weight = preg_replace('/\(/', '\(', $weight);
+	if(empty($weight))
+	{
+		$minweight = -1;
+		$maxweight = -1;
+		$wunit = NULL;
+	}
+	elseif($weight[1] != "" && $weight[2] != "" )
+	{
+		$minweight = $weight[1]; 
+		$maxweight = $weight[3];
+		$rest = preg_replace("/".$weight[1]." ?\- ?".$weight[3]." ?kg/", '', $rest);
+	}
+	elseif($weight[2] == "+" )
+	{
+		$minweight = $weight[3];
+		$maxweight = -1;
+		$rest = preg_replace("/\+ ?".$weight[3]." ?kg/", '', $rest);
+	}
+	elseif($weight[1] == "" && $weight[2] == "" )
+	{
+		$minweight = $weight[3];
+		$maxweight = $weight[3];
+		$rest = preg_replace("/".$weight[3]." ?kg/", '', $rest);
+	}
+	else
+	{
+		$minweight = -1;
+		$maxweight = $weight[3];
+		$rest = preg_replace("/\- ?".$weight[3]." ?kg/", '', $rest);
+	}
+	if (!empty($weight))
+	{
+		$wunit = $weight[4];
+	}
+
+	if(preg_match('/\+([0-9.]+)/', $rest, $weight) == 1)
+	{
+		$minweight = $weight[1];
+		$maxweight = -1;
+		$wunit = NULL;
+		$rest = preg_replace('/\+([0-9.]+)/', '', $rest);
+	}
+	if(preg_match('/([0-9.]+)-([0-9.]+)/', $rest, $weight) == 1)
+	{
+		$minweight = $weight[1];
+		$maxweight = $weight[2];
+		$wunit = NULL;
+		$rest = preg_replace('/[0-9.]+-[0-9.]+/', '', $rest);
+	}
+
+	if(preg_match('/(?<!\w)- ?([0-9.]+)/', $rest, $weight) == 1)
+	{
+		$maxweight = $weight[1];
+		$minweight = -1;
+		$wunit = NULL;
+		$rest = preg_replace('/- ?([0-9.]+)/', '', $rest);
+	}
+
+	$minweight = preg_replace('/\(/', '', $minweight);
+	$maxweight = preg_replace('/\(/', '', $maxweight);
+	$minweight = preg_replace('/\\\/', '', $minweight);
+	$maxweight = preg_replace('/\\\/', '', $maxweight);
+
+	// Distance
+	$dd = preg_match('/\(\d+ ?\w+ \+ \d+ ?\w+\)/', $rest, $dinfo);
+	$rest =	preg_replace('/\(\d+ ?\w+ \+ \d+ ?\w+\)/', '', $rest);	
+	$reg = '/(([NL]H\/)?([0-9\/,.x]+)) ?((km?|Mile|mile|metre|m|yd|yard|Yard)s?)/';
+	//$reg = '/(([NL]H\/)?(\d+[\/,.x]+)?\d+) ?((m|km|Mile|yd|yard)s?)/';
+	$tt = preg_match($reg , $rest, $dist);
+	if($tt == 0)
+	{
+		$dist = -1;
+		$dunit = NULL;
+	}
+	else
+	{
+		$rest = preg_replace($reg , '', $rest);
+		$distt = $dist[1];
+		$dunit = $dist[4];
+		$dist = $distt;
+		$dist = preg_replace('/(\d+)[ .](\d+)/', '$1,$2', $dist);
+		$dist = preg_replace('/( ?)(km?|Mile|mile|metre|m|yd|yard|Yard)(s?) /', ' $2', $dist); 
+		$dunit = preg_replace('/yard/i', 'yd', $dunit);
+	}
+	if ($dd)
+		$rest .= $dinfo[0];
+
+	// Team ?
+	$tt = preg_match('/(Team|Individual|Single|Double|Solo|relay)s?/i', $rest, $team);
+	if($tt > 1)
+	{
+		$team = $team[1];
+		$rest = preg_replace('/(Team|Individual|Single|Double|Solo|relay)s? ?/i', '', $rest);
+		$team = strtolower($team);
+		$team = ucfirst($team);
+	}
+	else
+	{
+		$team = NULL;
+	}
+	
+	// Remove sport
+	$spos = preg_split('/ |-/', $spo);
+	foreach ($spos as $spo)
+	{
+		$rest = preg_replace('/'.$spo.'/i', '', $rest);
+	}
+
+	// Category
+	preg_match('/(extra-?|Light ?|half ?|half-?)?\w+weighg?t/i', $rest, $cat);
+	if(isset($cat) && isset($cat[0]))
+	{
+		$cat = $cat[0];
+		$rest = preg_replace('/'.$cat.'/', '', $rest);
+		$cat = strtolower($cat);
+		$cat = ucfirst($cat);
+	}
+	else
+	{
+		$cat = NULL;
+	}
+
+	// Unit sanitize
+	if($dunit)
+		$dunit = preg_replace(array('/km?/i', '/metre|ms/i', '/miles?/i', '/yds?|yards?/i'), array('km', 'm', 'mi', 'yd'), $dunit);
+
+	// Rest sanitize
+	$rest = preg_replace('/at the \d{4} (winter|summer) olympics/i', '', $rest);
+	$rest = preg_replace('/ s$/', '', $rest); // Plural from units
+	$rest =	preg_replace(array('/  /','/[^a-z ]- [^a-z ]|^, | ,$|^,$|,|\( ?\)| $|^ /i'), array(' ', ''), $rest);
+	$rest = preg_replace('/^\)|^-/', '', $rest);
+	$rest = preg_replace(array('/\( /', '/ \)/'), array('(', ')'), $rest);
+	$rest = preg_replace('/^\(([^)]*)\)$/', '$1', $rest);
+	$rest = ucfirst($rest);
+	return array(
+			'dgender' => (int)$gender, 
+			'dminweight' => (int)$minweight,
+			'dmaxweight' => (int)$maxweight,
+			'dwunit' =>  $wunit, 
+			'ddist' => $dist,
+			'ddunit' => $dunit, 
+			'dteam' => $team, 
+			'dcat' => $cat, 
+			'drest' => $rest
+			);
+
+}
+
+
+
+?>
