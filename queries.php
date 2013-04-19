a27bcfa5ff59bda4efca9f8208b58b40beb26fcd
guillaume <guillaume.jornod@gmail.com>
2013-04-19 15:52:28 +0200

    Change encoding

--- queries.php
+++ queries.php
@@ -6,14 +6,14 @@ $_POST = $_GET;
 if (isset($_POST['action']) && $_POST['action'] == "top10" && isset($_POST['type']))
 {
 	$res = Athlete::getAthlete(10);		
-	$dom = new DomDocument("1.0", "UTF-8");
+	$dom = new DomDocument("1.0", "UCS-2");
 	$root = $dom->createElement('result');
 	foreach($res as $re)
 	{
 		foreach($re->listAttrib() as $key => $attrib)
 		{
 			$attrib = preg_replace('/ +$/', '', $attrib);
-			$attrib = htmlspecialchars($attrib, ENT_QUOTES, "UTF-8");
+			$attrib = utf8_encode($attrib);
 			$elem = $dom->createElement($key, $attrib);
 			$root->appendChild($elem);
 		}
